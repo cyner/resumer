528fdaa635a9cef865cd0f619d600ce3744528bb
Jan Votava <votava@deployment.cz>
2014-05-22 11:50:23 +0200

    Set default name from attachment filename, require name

--- backend/app/models/resume.rb
+++ backend/app/models/resume.rb
@@ -1,6 +1,17 @@
 class Resume < ActiveRecord::Base
   has_attached_file :cv
   validates_attachment_content_type :cv, content_type: ['application/pdf', 'application/msword', 'text/plain']
+  validate :name, presence: true
 
   belongs_to :job
+
+  before_create :set_name_from_cv_file_name
+
+  private
+  def set_name_from_cv_file_name
+    new_name = cv_file_name.chomp(File.extname(cv_file_name)).strip
+    new_name = "(unnamed)" unless new_name.present?
+
+    self.name ||= new_name
+  end
 end

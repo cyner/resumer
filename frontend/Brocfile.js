7417334ee51560e19f6efc95e16655b77098b92f
Jan Votava <votava@deployment.cz>
2014-05-30 11:47:32 +0200

    Add basic integration test for job#show

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -33,9 +33,9 @@ app.import('vendor/ic-ajax/dist/named-amd/main.js', {
   ]
 });
 
-//app.import({development:'vendor/route-recognizer/dist/route-recognizer.js'});
-//app.import({development:'vendor/FakeXMLHttpRequest/fake_xml_http_request.js'});
-//app.import({development:'vendor/pretender/pretender.js'});
+app.import({development:'vendor/route-recognizer/dist/route-recognizer.js'});
+app.import({development:'vendor/FakeXMLHttpRequest/fake_xml_http_request.js'});
+app.import({development:'vendor/pretender/pretender.js'});
 
 app.import('vendor/ember-validations/ember-validations.js');
 
---
+++ frontend/tests/integration/jobs-page-test.js
@@ -0,0 +1,43 @@
+import startApp from 'resumer/tests/helpers/start-app';
+
+var App, server;
+
+module('Integration - Jobs Page', {
+  setup: function() {
+    App = startApp();
+
+    var jobs = [
+      { id: 1, name: "First testing job" },
+      { id: 2, name: "Second testing job" },
+      { id: 3, name: "Best job ever" },
+      { id: 4, name: "Wassup?" }
+    ];
+
+    server = new Pretender(function() {
+      this.get('/api/jobs', function(request) {
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({jobs: jobs})];
+      });
+
+      this.post('/users/sign_in', function(request) {
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({user_token:"Tp7nVz6exkTMkWz1DEYk",user_email:"foo@bar.com"})];
+      });
+    });
+  },
+
+  teardown: function() {
+    Ember.run(App, 'destroy');
+    server.shutdown();
+  }
+});
+
+test('Should show the Job name after loging in', function() {
+  visit('/');
+  fillIn('input#identification', 'foo@bar.com');
+  fillIn('input#password', 'password');
+  click('button[type="submit"]');
+  visit('/job/1');
+
+  andThen(function() {
+    equal(find("h2:contains('First testing job')").length, 1);
+  });
+});
